0a7d678d1d7b889131959e1a869ef14923e9dbd0
salwamizar <salwasalwamoe2001@gmail.com>
2025-02-17 03:45:21 +0700

    adding virtual env + config login

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, session, redirect
+import paramiko
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+#login via ssh
+def test_login(ip, username, password):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=username, password=password)
+        ssh.close()
+        return True
+    except Exception as e:
+        return str(e)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -16,9 +31,24 @@ def index():
 def disable():
     return render_template("disable.html")
 
-@app.route("/login")
+@app.route("/login", method=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        ip = request.form.get("ip")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        login_result = test_login(ip, username, password)
+        if login_result is True:
+            session["ip"] = ip
+            session["username"] = username
+            return redirect("/dashboard")  # Redirect ke dashboard setelah login sukses
+        else:
+            return render_template("login.html", error="Login gagal, periksa IP, username, atau password.")
+
+
     return render_template("login.html")
+
 
 @app.route("/backup")
 def backup():
